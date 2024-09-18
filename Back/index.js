c609f7006e7701150255e896d63e7ef7fca1b769
Albert Pallach Font <albertpf@gmail.com>
2024-09-18 13:40:10 +0200

    Tests

--- Back/index.js
+++ Back/index.js
@@ -3,7 +3,7 @@ const express = require("express") /* Importem express per poder-lo utilitzar. *
 var app = express() /* Creem una variable per utilitzar express. */
 
 app.get("/", function (request, response) { /* Aquesta es la estructura bàsica d'una petició web. */
-    response.send("Hello World!")
+    response.send("Resposta activa!")
 })
 app.listen(10000, function () { /* Redirigim les peticions entrants d'un port concret. */
     console.log("Started application on port %d", 10000)
